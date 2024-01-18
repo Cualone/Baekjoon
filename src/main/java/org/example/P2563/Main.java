4a0a05303fa3f32d4f3b9bf10d6f4e40cd46d862
Cualone <june568@naver.com>
2024-01-18 17:36:30 +0900

    P2563

---
+++ src/main/java/org/example/P2563/Main.java
@@ -0,0 +1,30 @@
+package org.example.P2563;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count = 0;
+        boolean[][] rec = new boolean[100][100];
+        for (int i = 0; i < num; i++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            for (int j = n; j < n + 10; j++) {
+                for (int k = m; k < m + 10; k++) {
+                    rec[j][k] = true;
+                }
+            }
+        }
+
+        for (boolean[] i : rec) {
+            for (boolean j : i) {
+                if(j == true)
+                    count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
