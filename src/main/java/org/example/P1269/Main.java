a3d174809724534e52ccc1debe08121474668a1a
Cualone <june568@naver.com>
2024-02-16 02:56:55 +0900

    P1269

---
+++ src/main/java/org/example/P1269/Main.java
@@ -0,0 +1,38 @@
+package org.example.P1269;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int numA = Integer.parseInt(st.nextToken());
+        int numB = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        HashSet<Integer> A = new HashSet<>();
+        HashSet<Integer> B = new HashSet<>();
+
+        StringTokenizer eleA = new StringTokenizer(br.readLine());
+        for (int i = 0; i < numA; i++) {
+            A.add(Integer.parseInt(eleA.nextToken()));
+        }
+        StringTokenizer eleB = new StringTokenizer(br.readLine());
+        for (int i = 0; i < numB; i++) {
+            B.add(Integer.parseInt(eleB.nextToken()));
+        }
+        HashSet<Integer> comp = new HashSet<>(A);
+
+        comp.removeAll(B);
+        B.removeAll(A);
+
+        cnt = comp.size() + B.size();
+
+        System.out.println(cnt);
+    }
+}
