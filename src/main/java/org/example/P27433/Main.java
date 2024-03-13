8cc0fd3081865796d71048cdfdc238fb150a9485
Cualone <june568@naver.com>
2024-03-13 22:18:11 +0900

    P27433

---
+++ src/main/java/org/example/P27433/Main.java
@@ -0,0 +1,22 @@
+package org.example.P27433;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(facto(N));
+
+    }
+
+    static long facto(int num) {
+        if (num < 1) {
+            return 1;
+        }
+        return num * facto(num-1);
+    }
+}
