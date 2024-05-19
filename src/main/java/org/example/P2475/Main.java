edb0c2c4d45fd057217590feab71ec72588a5a67
Cualone <june568@naver.com>
2024-05-20 03:09:52 +0900

    P2475

---
+++ src/main/java/org/example/P2475/Main.java
@@ -0,0 +1,19 @@
+package org.example.P2475;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int result = 0;
+        for (int i=0; i<5; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            result += a*a;
+        }
+        System.out.println(result%10);
+    }
+}
