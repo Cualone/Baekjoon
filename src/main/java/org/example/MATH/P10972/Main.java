3693bddc56bc1f3dc60f38868fe119c24d7328d0
Cualone <june568@naver.com>
2024-06-23 22:17:06 +0900

    P10972

---
+++ src/main/java/org/example/MATH/P10972/Main.java
@@ -0,0 +1,58 @@
+package org.example.MATH.P10972;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = N - 1; i > 0; i--) {
+            if (arr[i] > arr[i - 1]) {
+                int upIdx = i;
+                for (int j = i; j < N; j++) {
+                    if (arr[j] > arr[i - 1] && arr[j] < arr[upIdx]) {
+                        upIdx = j;
+                    }
+                }
+
+                swap(i - 1, upIdx);
+
+                int start = i;
+                int end = N - 1;
+                while (start < end) {
+                    swap(start, end);
+                    start++;
+                    end--;
+                }
+
+                for (int k = 0; k < N; k++) {
+                    sb.append(arr[k]).append(" ");
+                }
+                System.out.println(sb);
+                return;
+            }
+
+        }
+
+        System.out.println(-1);
+    }
+
+    static void swap(int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
